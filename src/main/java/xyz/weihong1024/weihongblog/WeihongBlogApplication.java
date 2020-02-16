7279fbc877d2772550a7695a114b5266b9a196ee
BoFineHorseman <863535445@qq.com>
2020-02-16 23:59:06 +0800

    init repo

---
+++ src/main/java/xyz/weihong1024/weihongblog/WeihongBlogApplication.java
@@ -0,0 +1,13 @@
+package xyz.weihong1024.weihongblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeihongBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeihongBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/weihong1024/weihongblog/WeihongBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.weihong1024.weihongblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeihongBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
